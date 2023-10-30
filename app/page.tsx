7ab3488a23ef878d97f047d0d3d57091bc904db0
marvin <marvin010528@foxmail.com>
2023-10-30 08:56:28 +0800

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import Image from "next/image";
 import Link from "next/link";
-import { lusitana } from "@/app/ui/font";
+import { lusitana } from "@/app/ui/fonts";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
