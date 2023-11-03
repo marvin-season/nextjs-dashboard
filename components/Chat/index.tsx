7334aac4cdae44f730871178afa5cae154654fb1
marvin <marvin010528@foxmail.com>
2023-11-03 11:52:57 +0800

    send message keycode and scroll into view

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -1,27 +1,52 @@
 "use client";
 
-import { Button, Input } from "antd";
-import { KeyboardEventHandler, useState } from "react";
+import { Button, Input, InputRef, message } from "antd";
+import { KeyboardEventHandler, useEffect, useRef, useState } from "react";
 import { MessageList } from "react-chat-elements";
 import "react-chat-elements/dist/main.css";
 
 interface ChatProps {
-  dataSource: any;
+  dataSource: any[];
   onSend: (text: string) => string;
 }
 
 export default function Chat({ dataSource, onSend }: ChatProps) {
+  const [messageApi, contextHolder] = message.useMessage();
   const [text, setText] = useState("");
-  const handleKeyUp: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    e.key === "Enter" && onSend(text) && setText("");
+  const messageListRef = useRef<HTMLElement>(null);
+  const inputRef = useRef<InputRef>(null);
+
+
+  const handleSendMessage = () => {
+    if (!text) {
+      messageApi.open({
+        type: "warning",
+        content: "input empty!",
+      });
+      inputRef.current?.focus();
+      return;
+    }
+
+    onSend(text);
+    setText("");
   };
+
+  useEffect(() => {
+    if (dataSource.length > 0) {
+      messageListRef.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [dataSource.length]);
 
   return (
     <>
+      {contextHolder}
       <div className="">
         <div className="flex flex-col gap-4">
           <MessageList
-            referance={null}
+            referance={messageListRef}
             className="message-list h-96 bg-gradient-to-r from-indigo-200 rounded-md p-2"
             lockable={true}
             toBottomHeight={"100%"}
@@ -29,21 +54,18 @@ export default function Chat({ dataSource, onSend }: ChatProps) {
           />
           <div className="flex flex-row gap-10 justify-between">
             <Input
+              ref={inputRef}
               value={text}
               placeholder="Type here..."
               onChange={(e: any) => {
                 setText(e.target.value);
               }}
-              onKeyUp={handleKeyUp}
+              onKeyUp={(e) => {
+                e.key === "Enter" && handleSendMessage();
+              }}
             />
 
-            <Button
-              onClick={() => {
-                onSend(text);
-                setText("");
-              }}
-              title="Send"
-            >
+            <Button onClick={handleSendMessage} title="Send">
               send
             </Button>
           </div>
